502e7617fc7d1cf20cb6d9647d201f0b5868a464
Irina <Irysichka2704@gmail.com>
2025-07-07 19:35:22 +0300

    init

--- src/main.js
+++ src/main.js
@@ -15,4 +15,65 @@
   </li>
 */
 
+// Ключи
+const STORAGE_KEY_TASKS = 'todo_tasks_v2';
+const STORAGE_KEY_THEME = 'todo_theme_v2';
+
+// DOM елементи
+const form = document.querySelector(".header-form");
+const inputName = document.querySelector('input[name="taskName"]');
+const taskList = document.querySelector("#task-list");
+const inputDesc = document.querySelector('input[name="taskDescription"]');
+const changeTheme = document.querySelector("#themeToggle");
+const body=document.body;
+
+function loadTasks() {
+  try {
+    return JSON.parse(localStorage.getItem(STORAGE_KEY_TASKS)) || []
+  }
+  catch (_) {
+   return []
+  }
+}
+
+
+
+
+
+// form.addEventListener("input", handleInput);
+
+const newName = refs.inputName.value.trim();
+const newTask = refs.inputDesc.value.trim();
+
+
+form.addEventListener("submit", handleSubmit)
+
+function handleSubmit(e) {
+    e.preventDefault();
+    refs.form.reset()
+}
+
+const getEl =  `<li class="task-list-item">
+      <button class="task-list-item-btn">Delete</button>
+      <h3>${newName}</h3>
+      <p>${newTask}</p>
+  </li>`
+
+
+changeTheme.addEventListener("click", handleClick);
+
+function handleClick() {
+    if (body.classList.contains("theme-dark")) { 
+        body.classList.remove("theme-dark");
+        body.classList.add("theme-light");
+        localStorage.setItem("theme", "theme-light");
+    } else {
+        body.classList.remove("theme-light");
+        body.classList.add("theme-dark");
+        localStorage.setItem("theme", "theme-dark");
+    }
+   
+}
+
+
 
